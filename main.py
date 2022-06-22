283c512a20a372ea7816a39c02cdee8cdf522dc5
robertay <94378311+robyuk@users.noreply.github.com>
2022-06-22 13:48:40 +0000

    Added comments

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 root=Path('files')
 
+# Changes the suffix of all files in a directory
+# Use glob or rglob instead of iterdir to recursively # change all files in a directory tree
+
 for path in root.iterdir():
   if path.is_file:
     newname=path.stem + '.csv'
